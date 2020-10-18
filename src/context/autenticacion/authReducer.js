d0825c65288c5a6cab6b186c1ca2645249a99a66
unknown <aarguelles05@gmail.com>
2020-10-19 00:29:58 +0200

    Adding state and provider in auth

---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,9 @@
+import {REGISTRO_EXISTOSO, REGISTRO_ERROR, OBTENER_USUARIO, LOGIN_EXISTOSO, LOGIN_ERROR, CERRAR_SESION} from '../../types/index';
+
+export default (state, action) => {
+    switch(action.type){
+        
+        default:
+            return state;
+    }
+}
